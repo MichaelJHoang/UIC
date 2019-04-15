c2ae9286d2d3df6faab101feb37118c5d9c6a2ae
Michael Hoang <MichaelJHoang@gmail.com>
2019-04-15 09:47:32 -0500

    updated gopher game

--- uic/CS 478 - Software Development for Mobile Platforms/CS478Project4/app/src/main/java/com/example/cs478project4/CustomAdapter.java
+++ uic/CS 478 - Software Development for Mobile Platforms/CS478Project4/app/src/main/java/com/example/cs478project4/CustomAdapter.java
@@ -2,27 +2,60 @@ package com.example.cs478project4;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
-public class CustomAdapter extends ArrayAdapter
+public class CustomAdapter extends BaseAdapter
 {
-    private int images[];
+    private int images [];
 
     private Context context;
 
-    private LayoutInflater layoutInflater;
+    private LayoutInflater inflater;
 
-    public CustomAdapter()
+    public CustomAdapter (Context context, int images[])
     {
-
+        this.context = context;
+        this.images = images;
     }
 
     @Override
-    public int getCount()
+    public int getCount ()
     {
-        return 0;
+        return images.length;
     }
 
-    @
+    @Override
+    public Object getItem (int position)
+    {
+        return images[position];
+    }
+
+    @Override
+    public long getItemId (int position)
+    {
+        return position;
+    }
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent)
+    {
+        View gridView = convertView;
+
+        if (convertView == null)
+        {
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            gridView = inflater.inflate(R.layout.custom_layout, null);
+        }
+
+        ImageView holeImage = (ImageView) gridView.findViewById(R.id.customLayoutImage);
+
+        holeImage.setImageResource(images[position]);
+
+        return gridView;
+    }
 }
--- uic/CS 478 - Software Development for Mobile Platforms/CS478Project4/app/src/main/java/com/example/cs478project4/GameActivity.java
+++ uic/CS 478 - Software Development for Mobile Platforms/CS478Project4/app/src/main/java/com/example/cs478project4/GameActivity.java
@@ -2,14 +2,42 @@ package com.example.cs478project4;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity
 {
+    GridView gridView;
+
+    int holeImages [] = new int [100];
+
     @Override
     public void onCreate (Bundle saved)
     {
+        for (int x = 0; x < 100; x++)
+        {
+            holeImages[x] = R.drawable.dirt_hole;
+        }
+
         // basic required stuff
         super.onCreate(saved);
         setContentView(R.layout.activity_game);
+
+        gridView = (GridView) findViewById(R.id.game_gridView);
+
+        CustomAdapter adapter = new CustomAdapter (GameActivity.this, holeImages);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(GameActivity.this, "Hole " + position + " Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
